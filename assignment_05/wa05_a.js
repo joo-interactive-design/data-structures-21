5a1a5d20032e7bfe4967d610c0bb86edf4c32fc7
EC2 Default User <ec2-user@ip-172-31-5-252.us-east-2.compute.internal>
2021-10-19 19:29:09 +0000

    assignment_05

--- assignment_05/wa05_a.js
+++ assignment_05/wa05_a.js
@@ -1,45 +1,52 @@
 var blogEntries = [];
 
 class BlogEntry {
-  constructor(primaryKey, restaurantOrCafe, name, visitDate, address, recommend, comment) {
+  constructor(primaryKey, colorPalette01, colorPalette02, colorPalette03, colorPalette04, place, date, image, comment) {
     this.pk = {};
     this.pk.N = primaryKey.toString(); //number->string
-    this.name = {};
-    this.name.S = restaurantOrCafe;
-    this.address = {};
-    this.address.S = address;
-    this.visitDate = {}; 
-    this.visitDate.S = new Date(visitDate).toDateString();// convert to javascript date format
-    this.address = {};
-    this.address.S = address;
-    this.recommend = {};
-    this.recommend.BOOL = recommend; 
+    this.colorPalette01 = {};
+    this.colorPalette01.S = colorPalette01;
+    this.colorPalette02 = {};
+    this.colorPalette02.S = colorPalette02;
+    this.colorPalette03 = {};
+    this.colorPalette03.S = colorPalette03;
+    this.colorPalette04 = {};
+    this.colorPalette04.S = colorPalette04;
+    this.place = {};
+    this.place.S = place;
+    this.date = {}; 
+    this.date.S = new Date(date).toDateString();// convert to javascript date format
+    this.image= {};
+    this.image.S = image;
     if (comment != null) {
         this.comment = {};
         this.comment.S = comment; 
     }
+    
   }
 }
 
-blogEntries.push(new BlogEntry(0, 'cafe', 'paper coffee', 'October 01, 2021', '44 W 29th St, New York, NY 10001', true, 'I loved the wooden interior, which is very cozy! small coffee spot inside boutique hotel Made.'));
-blogEntries.push(new BlogEntry(1, 'restaurant', 'AOI Kitchen', 'October 03, 2021', '320 E 6th St, New York, NY 10003', true, 'One of the most unique Japanese place I’ve been to in the east village.'));
-blogEntries.push(new BlogEntry(2, 'restaurant', "ruby's", 'October 15, 2021', '442 3rd Ave, New York, NY', true, 'Great brunch spot'));
-blogEntries.push(new BlogEntry(3, 'cafe', "Eileen's Special Cheesecake", 'October 10, 2021', '17 Cleveland Pl, New York, NY 10012', true, 'Best cheese cake, ever.'));
+blogEntries.push(new BlogEntry(0, '#808ca4', '#9d8488', '#d4907d', '#fea67a', 'Tokyo,Japan', 'March 13, 2021', 'https://drive.google.com/drive/u/0/folders/1KRGQ93NGK9PMdQ_bhDOWO-eoeZLjdSIc', 'on the way to home after working!'))
+blogEntries.push(new BlogEntry(1, '#60748f', '#8995a5', '#abb9c6', '#c5d0d4', 'Tokyo,Japan', 'April 08, 2021', 'https://drive.google.com/drive/u/0/folders/1KRGQ93NGK9PMdQ_bhDOWO-eoeZLjdSIc', 'Heading back to Korea after family trip'))
+blogEntries.push(new BlogEntry(2, '#fbb091', '#afcdd7', '#6f8698', '#dcd1b5', 'Tokyo,Japan', 'May 05, 2021', 'https://drive.google.com/drive/u/0/folders/1KRGQ93NGK9PMdQ_bhDOWO-eoeZLjdSIc'))
+blogEntries.push(new BlogEntry(3, '#db7a57', '#f9ad59', '#b9b9b7', '#fcb479', 'Tokyo,Japan', 'May 10, 2021', 'https://drive.google.com/drive/u/0/folders/1KRGQ93NGK9PMdQ_bhDOWO-eoeZLjdSIc'))
+
 
 console.log(blogEntries);
 
 
 var AWS = require('aws-sdk');
 AWS.config = new AWS.Config();
-AWS.config.region = "us-east-2a";
+AWS.config.region = "us-east-2";
 
 var dynamodb = new AWS.DynamoDB();
 
-var params = {};
-params.Item = blogEntries[0]; 
+for (var i=0; i< blogEntries.length; i++){ var params = {};
+params.Item = blogEntries[i]; 
 params.TableName = "processblog";
 
 dynamodb.putItem(params, function (err, data) {
   if (err) console.log(err, err.stack); // an error occurred
   else     console.log(data);           // successful response
 });
+}
