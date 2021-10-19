f11e53f7d923cf78cac24f039dcbc5a923982bab
EC2 Default User <ec2-user@ip-172-31-5-252.us-east-2.compute.internal>
2021-10-19 20:43:38 +0000

    assignment_06

---
+++ assignment_06/wa06_aa.js
@@ -0,0 +1,26 @@
+const { Client } = require('pg');
+const cTable = require('console.table');
+
+// AWS RDS POSTGRESQL INSTANCE
+var db_credentials = new Object();
+db_credentials.user = 'joojoo';
+db_credentials.host = 'ds-fall-21.cfbwimnbp6s8.us-east-2.rds.amazonaws.com';
+db_credentials.database = 'aa';
+db_credentials.password = process.env.AWSRDS_PW;
+db_credentials.port = 5432;
+
+
+// Connect to the AWS RDS Postgres database
+const client = new Client(db_credentials);
+client.connect();
+
+// Sample SQL statement to query meetings on Monday that start on or after 7:00pm: 
+var thisQuery = "SELECT mtgday, mtgtime, mtglocation, mtgaddress, mtgtypes FROM aadata WHERE mtgday = 'Monday' and mtghour >= 19;";
+
+client.query(thisQuery, (err, res) => {
+    if (err) {throw err}
+    else {
+        console.table(res.rows);
+        client.end();
+    }
+});
